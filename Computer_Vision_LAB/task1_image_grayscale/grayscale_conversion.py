4d23d2aba1ec66465a4c4a25cdcc275c4cf61b0d
Akunorsalim <siakunor@st.ug.edu.gh>
2025-08-01 12:53:31 +0000

    GrayScale Conversion

--- Computer_Vision_LAB/task1_image_grayscale/grayscale_conversion.py
+++ Computer_Vision_LAB/task1_image_grayscale/grayscale_conversion.py
@@ -1,1 +1,17 @@
-print("Gray Scale")
+import cv2
+
+# Load the original image
+image = cv2.imread('../photo.jpg')  # Adjust path if needed
+cv2.imshow("Original Image", image)
+
+# Convert to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Grayscale Image", gray_image)
+
+# Save grayscale image
+
+cv2.imwrite('photo_gray.jpg', gray_image)
+
+# Wait and close windows
+cv2.waitKey(0)
+cv2.destroyAllWindows()
